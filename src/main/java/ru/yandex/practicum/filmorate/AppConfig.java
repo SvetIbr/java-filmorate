fe36e2d211e9e179038a92764b6dfa990f3f8faf
Svetlana <tchmykova@yandex.ru>
2023-08-16 17:09:39 +0500

    fix: formatted

--- src/main/java/ru/yandex/practicum/filmorate/AppConfig.java
+++ src/main/java/ru/yandex/practicum/filmorate/AppConfig.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -22,29 +20,29 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
-@PropertySource(value = { "classpath:application.properties" })
+@PropertySource(value = {"classpath:application.properties"})
 public class AppConfig {
 
-    //private Environment env;
     @Value("${init-db:false}")
     private String initDatabase;
+
     @Bean
-    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer()
-    {
+    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
+
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
+
     @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource)
-    {
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:file:./db/filmorate");
@@ -52,9 +50,9 @@ public class AppConfig {
         dataSource.setPassword("password");
         return dataSource;
     }
+
     @Bean
-    public DataSourceInitializer dataSourceInitializer(DataSource dataSource)
-    {
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
         dataSourceInitializer.setDataSource(dataSource);
         ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
